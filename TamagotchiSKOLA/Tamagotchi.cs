91f4bddc1577d25428b2e8d7f13b7e0ec914ecf2
Henry Eriksson <eriksson.henry@elev.ga.ntig.se>
2022-09-27 12:15:32 +0200

    Tamagotchi

---
+++ TamagotchiSKOLA/Tamagotchi.cs
@@ -0,0 +1,11 @@
+public class Tamagotchi
+{
+    int hunger = 100;
+    int boredom = 100;
+    List<string> phrases = new List<string>(){"I want crack!", "Playtime.", "Hihi"};
+    bool isAlive = true;
+    Random rand = new Random(10);
+    String name = slaveFORchildren
+
+    
+}
